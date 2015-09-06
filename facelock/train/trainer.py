a9894e72f3cade4f2c3d28befebb280ba0a7ab82
Yasyf Mohamedali <yasyfm@gmail.com>
2015-09-06 01:18:10 -0400

    Add demo of model prediction.

--- facelock/train/trainer.py
+++ facelock/train/trainer.py
@@ -6,26 +6,35 @@ class Trainer(object):
   FACE_WIDTH = 120.0
   FACE_HEIGHT = 140.0
 
-  def __init__(self, positives, negatives):
+  def __init__(self, positives=(), negatives=()):
     self.positives = positives
     self.negatives = negatives
 
-  def crop_to_face(self, photo, face):
+  @classmethod
+  def crop_to_face(cls, photo, face):
     (x, y, width, height) = face
-    crop_height = (width / self.FACE_WIDTH) * self.FACE_HEIGHT
+    crop_height = (width / cls.FACE_WIDTH) * cls.FACE_HEIGHT
     middle = y + (height / 2)
     y1 = max(0, middle - (crop_height / 2))
     y2 = min(photo.height - 1, middle + (crop_height / 2))
     return photo.crop(x, x + width, y1, y2)
 
+  @classmethod
+  def process(cls, photo):
+    face = photo.detect_face()
+    if face is None:
+      return None
+    return cls.crop_to_face(photo, face).resize(cls.FACE_WIDTH, cls.FACE_HEIGHT)
+
   @photo_collection
   def processed_positives(self):
     for photo in self.positives:
-      face = photo.detect_face()
-      if face is None:
+      processed = self.process(photo)
+      if processed is None:
         # Skip if no or multiple faces
         continue
-      yield self.crop_to_face(photo, face).resize(self.FACE_WIDTH, self.FACE_HEIGHT)
+      else:
+        yield processed
     raise StopIteration
 
   @photo_collection
@@ -40,9 +49,15 @@ class Trainer(object):
     labels.extend([1] * len(faces))
 
     faces.extend(negatives.call('raw'))
-    labels.extend([-1] * (len(faces) - len(labels)))
+    labels.extend([0] * (len(faces) - len(labels)))
 
     model = cv2.createEigenFaceRecognizer()
     model.train(np.asarray(faces), np.asarray(labels))
 
+    return model
+
+  @staticmethod
+  def load(path):
+    model = cv2.createEigenFaceRecognizer()
+    model.load(path)
     return model
--- main.py
+++ main.py
@@ -4,28 +4,57 @@ from facelock.config import Config
 from facelock.cv.photo import Photo
 import os
 import re
+import cv2
+import sys
 
+OUTPUT_DIR = 'tmp'
 USER_ID = 'YasyfM'
 N = 10
-NEGATIVE_SAMPLE_FOLDER = 'yalefaces/'
+NEGATIVE_SAMPLE_FOLDER = 'yalefaces'
 NEGATIVE_SAMPLE_REGEX = '\.png'
+MODEL_NAME = 'model.xml'
 
 def save_raw(photos):
-  photos.save_n(N, 'tmp/raw/{user_id}', user_id=USER_ID)
+  photos.save_n(N, '{out}/raw/{user_id}', out=OUTPUT_DIR, user_id=USER_ID)
 
 def save_preprocessed(photos):
-  Trainer(photos.call('to_cv'), []).processed_positives().save_n(N, 'tmp/preprocessed/{user_id}', user_id=USER_ID)
+  processed = Trainer(photos.call('to_cv')).processed_positives()
+  processed.save_n(N, '{out}/preprocessed/{user_id}', out=OUTPUT_DIR, user_id=USER_ID)
 
 def negative_samples():
   directory = Config.check_filename(NEGATIVE_SAMPLE_FOLDER)
   files = os.listdir(directory)
   return (Photo.from_path(os.path.join(directory, fn)) for fn in files if re.search(NEGATIVE_SAMPLE_REGEX, fn))
 
+def capture_image():
+  capture = cv2.VideoCapture(0)
+  _, frame = capture.read()
+  capture.release()
+
+  image = Photo(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
+  return Trainer.process(image)
+
 if __name__ == '__main__':
+  if len(sys.argv) != 2:
+    raise RuntimeError('Incorrect number of arguments!')
+
   graph = Graph.for_user(USER_ID)
   photos = graph.photos()
-  # save_raw(photos)
-  # save_preprocessed(photos)
-  trainer = Trainer(photos.limit(N).call('to_cv'), negative_samples())
-  model = trainer.train()
-  model.save('tmp/model.xml')
+
+  if sys.argv[1] == '--save-raw':
+    save_raw(photos)
+  elif sys.argv[1] == '--save-processed':
+    save_preprocessed(photos)
+  elif sys.argv[1] == '--train':
+    trainer = Trainer(photos.limit(N).call('to_cv'), negative_samples())
+    model = trainer.train()
+    model.save('{out}/{model}'.format(out=OUTPUT_DIR, model=MODEL_NAME))
+  elif sys.argv[1] == '--predict':
+    model = Trainer.load('{out}/{model}'.format(out=OUTPUT_DIR, model=MODEL_NAME))
+    image = capture_image()
+    if image is None:
+      raise RuntimeError('No face detected!')
+    else:
+      label, confidence = model.predict(image.raw())
+      print 'Predicted {label} with confidence of {confidence}!'.format(label=bool(label), confidence=confidence)
+      image.show()
